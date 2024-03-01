03e1761122ce05633975ba2facbaf46ea8c35ab6
boostaboosh <mail.laurencesnow@gmail.com>
2024-03-01 12:27:30 +0000

    Add public interface to RestaurantViewer class

---
+++ top-ten-restaurant-viewer/src/RestaurantViewer.java
@@ -0,0 +1,23 @@
+/**
+ * The Restaurant viewer class containes a program which displays restaurant
+ * information to the user.
+ */
+public class RestaurantViewer
+{
+   /**
+    * Displays restaurant information.
+    */
+   public void displayRestaurants()
+   {
+      // TODO: fill implementation
+   }
+
+   /**
+    * The method is the program starting point.
+    * @param args command line arguments
+    */
+   public static void main(String[] args)
+   {
+      // TODO: fill implementation
+   }
+}
