016d0f55ba394906e7647545f4bdcd4e77d6f923
boostaboosh <mail.laurencesnow@gmail.com>
2024-03-01 14:09:44 +0000

    Update RestaurantDataFetcher public interface

--- top-ten-restaurant-viewer/src/RestaurantDataFetcher.java
+++ top-ten-restaurant-viewer/src/RestaurantDataFetcher.java
@@ -6,36 +6,29 @@ import java.util.ArrayList;
 public class RestaurantDataFetcher
 {
    // instance variables
-   private int numberOfRestaurantsToFetch;
 
    // constructors
    /**
     * Constructs a restaurant fetcher object by initialising its variables.
     */
-   public RestaurantDataFetcher(int numberOfRestaurantsToFetch)
+   public RestaurantDataFetcher()
    {
-      this.numberOfRestaurantsToFetch = numberOfRestaurantsToFetch;
    }
 
    // methods
    /**
-    * Sets the number of restaurants to fetch.
-    * @param numberOfRestaurantsToFetch the number of restaurants to fetch
-    */
-   public void setNumberOfRestaurantsToFetch(int numberOfRestaurantsToFetch)
-   {
-      this.numberOfRestaurantsToFetch = numberOfRestaurantsToFetch;
-   }
-
-   /**
     * Fetches the restaurant data of restaurants at the specified postcode,
     * and returns an array list of restaurant objects.
     * @param postcode the postcode to search for restaurant data
+    * @param numberOfRestaurantsToFetch the number of restaurants to fetch
     * @return an array list of restaurant objects
     */
-   public ArrayList<Restaurant> fetchRestaurants(String postcode)
+   public ArrayList<Restaurant> fetchRestaurants(String postcode, int numberOfRestaurantsToFetch)
    {
       // TODO: fill implementation
+      // make HTTP GET request to the Just Eat API endpoint
+      // parse the JSON object response from the Just Eat API endpoint
+      // create a list of restaurants of numberOfRestaurantsToFetch length
       return new ArrayList<>();
    }
 }
--- top-ten-restaurant-viewer/src/RestaurantViewer.java
+++ top-ten-restaurant-viewer/src/RestaurantViewer.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 /**
- * The Restaurant viewer class containes a program which displays restaurant
+ * The Restaurant viewer class contains a program which displays restaurant
  * information to the user.
  */
 public class RestaurantViewer
@@ -13,8 +13,8 @@ public class RestaurantViewer
    public static void main(String[] args)
    {
       final String POSTCODE = "FK94LJ"; // e.g.EC4M7RF
-      RestaurantDataFetcher restaurantDataFetcher = new RestaurantDataFetcher(10);
-      ArrayList<Restaurant> restaurants = restaurantDataFetcher.fetchRestaurants(POSTCODE);
+      RestaurantDataFetcher restaurantDataFetcher = new RestaurantDataFetcher();
+      ArrayList<Restaurant> restaurants = restaurantDataFetcher.fetchRestaurants(POSTCODE, 10);
       RestaurantViewer restaurantViewer = new RestaurantViewer();
       restaurantViewer.displayRestaurants(restaurants);
    }
